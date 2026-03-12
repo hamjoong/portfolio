708cf012064e769f4217d609c231988b286ea245
hamjoong <hamjoong@google.com>
2026-03-12 11:38:50 +0900

    fix: use relative base paths for sub-folder deployment

--- blog3/vite.config.ts
+++ blog3/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 비트 설정 정보: https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/blog3/',
+  base: './',
   plugins: [react()],
 })
