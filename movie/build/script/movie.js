8d3797218492783b6277163f4ac64eeafe0438ee
hamjoong <hamjoong@google.com>
2026-03-20 10:24:02 +0900

    movie

--- movie/build/script/movie.js
+++ movie/build/script/movie.js
@@ -1,249 +1,79 @@
-/*무비 메뉴바 슬라이드 영역*/
+/* 무비 메뉴바 슬라이드 영역 */
 $('header nav > ul').mouseenter(function() {
-  $('ul.sub_nav').stop().slideDown(500)
-})
+  $('ul.sub_nav').stop().slideDown(500);
+});
 
 $('header nav > ul').mouseleave(function() {
-  $('ul.sub_nav').stop().slideUp(300)
-})
+  $('ul.sub_nav').stop().slideUp(300);
+});
 
-/*무비 무비차트,상영예정 클릭시 리스트 변경영역*/
-$('.movie_font').click( () => {
-  $('.movie_img_box2').hide();
-  $('.movie_img_box').show();
-  $('.movie_font').css('background-color', 'rgb(230,230,230)');
-  $('.movie_font').css('color', '#555'); 
-  $('.movie_img_box').css('background-color', 'rgb(230,230,230)');
-  $('.movie_font2').css('background-color', '#FFF');
-  $('.movie_font2').css('color', 'rgb(80,80,80)');  
-})
+/* 무비 무비차트, 상영예정 클릭 시 리스트 변경 영역 */
+$('.movie_font, .movie_font2').click(function() {
+  const isChart2 = $(this).hasClass('movie_font2');
+  
+  $('.movie_img_box').toggle(!isChart2);
+  $('.movie_img_box2').toggle(isChart2);
+  
+  $('.movie_font').css({
+    'background-color': isChart2 ? '#FFF' : 'rgb(230,230,230)',
+    'color': isChart2 ? 'rgb(80,80,80)' : '#555'
+  });
+  
+  $('.movie_font2').css({
+    'background-color': isChart2 ? 'rgb(230,230,230)' : '#FFF',
+    'color': isChart2 ? '#555' : 'rgb(80,80,80)'
+  });
 
-$('.movie_font2').click( () => {
-  $('.movie_img_box').hide();
-  $('.movie_img_box2').show();
-  $('.movie_font2').css('background-color', 'rgb(230,230,230)');
-  $('.movie_font2').css('color', '#555'); 
-  $('.movie_img_box2').css('background-color', 'rgb(230,230,230)');
-  $('.movie_font').css('background-color', '#FFF');
-  $('.movie_font').css('color', 'rgb(80,80,80)');  
-})
+  if (isChart2) {
+    $('.movie_img_box2').css('background-color', 'rgb(230,230,230)');
+  } else {
+    $('.movie_img_box').css('background-color', 'rgb(230,230,230)');
+  }
+});
 
-/*무비 이미지 클릭시 영상 변경 영역*/
-$('.movie_img').click( () => {
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box').show();
-})
+/* 무비 이미지 클릭 시 영상 변경 영역 */
+$('.img_box article, .img_box2 article').click(function(e) {
+  e.stopPropagation();
+  const className = $(this).attr('class');
+  // 정규표현식 수정: 클래스명 내의 movie_img 숫자를 더 정확하게 추출
+  const match = className.match(/movie_img(\d+)/);
+  const num = match ? match[1] : "";
+  
+  $('.movie_trailer_box article').hide();
+  // num이 비어있으면 첫 번째 trailer_box (클래스명이 trailer_box 인 것) 표시
+  if (num === "" || parseInt(num) <= 1) {
+    $('.trailer_box').show();
+  } else {
+    $(`.trailer_box${num}`).show();
+  }
+});
 
-$('.movie_img2').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box2').show();
-})
-
-$('.movie_img3').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box3').show();
-})
-
-$('.movie_img4').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box4').show();
-})
-
-$('.movie_img5').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box5').show();
-})
-
-$('.movie_img6').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box6').show();
-})
-
-$('.movie_img7').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box7').show();
-})
-
-
-$('.movie_img8').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box8').show();
-})
-
-$('.movie_img9').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box9').show();
-})
-
-$('.movie_img10').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box10').show();
-})
-
-$('.movie_img11').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box12').hide();
-  $('.trailer_box11').show();
-})
-
-$('.movie_img12').click( () => {
-  $('.trailer_box').hide();
-  $('.trailer_box2').hide();
-  $('.trailer_box3').hide();
-  $('.trailer_box4').hide();
-  $('.trailer_box5').hide();
-  $('.trailer_box6').hide();
-  $('.trailer_box7').hide();
-  $('.trailer_box8').hide();
-  $('.trailer_box9').hide();
-  $('.trailer_box10').hide();
-  $('.trailer_box11').hide();
-  $('.trailer_box12').show();
-})
-
-/*무비 페이지 변경 영역*/
-$('.nav1').click( () => {
+/* 무비 페이지 변경 영역 */
+$('.nav1').click(() => {
   $('main').hide();
   $('.mini_box').show();
   $('.movie_chaert_expected_box2').hide();
-})
+});
 
-$('.header_font2').click( () => {
+$('.header_font2').click(() => {
   $('.mini_box').hide();
   $('main').show();
-})
+});
 
-/*무비 미니 헤더 폰트 영역 클릭시 색상 변경 영역*/
-$('.mini_header_font').click( () => {
-  $('.mini_header_font').css('background-color', 'rgb(255,192,203)')
-  $('.mini_header_font').css('color', '#555')
-  $('.mini_header_font2').css('background-color', '#555')
-  $('.mini_header_font2').css('color', '#FFF') 
-})
-
-$('.mini_header_font2').click( () => {
-  $('.mini_header_font2').css('background-color', 'rgb(255,192,203)')
-  $('.mini_header_font2').css('color', '#555')
-  $('.mini_header_font').css('background-color', '#555')
-  $('.mini_header_font').css('color', '#FFF') 
-})
-
-/*무비 미니 헤더 폰트 클릭시 LIST 바뀌는 영역*/
-$('.mini_header_font').click( () => {
-  $('.movie_chaert_expected_box2').hide();
-  $('.movie_chaert_expected_box').show();
-})
-
-$('.mini_header_font2').click( () => {
-  $('.movie_chaert_expected_box').hide();
-  $('.movie_chaert_expected_box2').show();
-})
+/* 무비 미니 헤더 폰트 영역 클릭 시 색상 및 리스트 변경 영역 */
+$('.mini_header_font, .mini_header_font2').click(function() {
+  const isExpected = $(this).hasClass('mini_header_font2');
+  
+  $('.mini_header_font').css({
+    'background-color': isExpected ? '#555' : 'rgb(255,192,203)',
+    'color': isExpected ? '#FFF' : '#555'
+  });
+  
+  $('.mini_header_font2').css({
+    'background-color': isExpected ? 'rgb(255,192,203)' : '#555',
+    'color': isExpected ? '#555' : '#FFF'
+  });
+  
+  $('.movie_chaert_expected_box').toggle(!isExpected);
+  $('.movie_chaert_expected_box2').toggle(isExpected);
+});
