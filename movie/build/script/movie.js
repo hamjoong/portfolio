b32b67d40743d8b7be825e1a129934eee5b13bbd
hamjoong <hamjoong@google.com>
2026-09-17 02:50:47 +0900

    Movie Box README modify

--- movie/build/script/movie.js
+++ movie/build/script/movie.js
@@ -7,6 +7,14 @@ $('header nav > ul').mouseleave(function() {
   $('ul.sub_nav').stop().slideUp(300);
 });
 
+// 재생 중인 모든 유튜브 iframe 영상 일시 정지/초기화 함수
+function stopAllTrailers() {
+  $('.movie_trailer_box iframe').each(function() {
+    const src = $(this).attr('src');
+    $(this).attr('src', src);
+  });
+}
+
 /* 무비 무비차트, 상영예정 클릭 시 리스트 변경 영역 */
 $('.movie_font, .movie_font2').click(function() {
   const isChart2 = $(this).hasClass('movie_font2');
@@ -29,18 +37,28 @@ $('.movie_font, .movie_font2').click(function() {
   } else {
     $('.movie_img_box').css('background-color', 'rgb(230,230,230)');
   }
+
+  // 탭 전환 시 기존 재생 중이던 예고편 정지 및 기본 예고편으로 리셋
+  stopAllTrailers();
+  $('.movie_trailer_box article').hide();
+  if (isChart2) {
+    $('.trailer_box7').show(); // 상영예정작 첫 번째 영상
+  } else {
+    $('.trailer_box').show();  // 무비차트 첫 번째 영상
+  }
 });
 
 /* 무비 이미지 클릭 시 영상 변경 영역 */
 $('.img_box article, .img_box2 article').click(function(e) {
   e.stopPropagation();
   const className = $(this).attr('class');
-  // 정규표현식 수정: 클래스명 내의 movie_img 숫자를 더 정확하게 추출
+  // 정규표현식: 클래스명 내의 movie_img 숫자를 추출
   const match = className.match(/movie_img(\d+)/);
   const num = match ? match[1] : "";
   
+  stopAllTrailers();
   $('.movie_trailer_box article').hide();
-  // num이 비어있으면 첫 번째 trailer_box (클래스명이 trailer_box 인 것) 표시
+  // num이 비어있거나 1이면 첫 번째 trailer_box 표시
   if (num === "" || parseInt(num) <= 1) {
     $('.trailer_box').show();
   } else {
