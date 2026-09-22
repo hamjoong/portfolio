fffbebe714dc188b2b1c0d180bbff60d0053b97c
hamjoong <hamjoong@google.com>
2026-09-22 18:00:28 +0900

    shoppingmall

--- shoppingmall/backend/src/main/java/com/projectx/auth/service/EncryptionService.java
+++ shoppingmall/backend/src/main/java/com/projectx/auth/service/EncryptionService.java
@@ -5,6 +5,7 @@ import com.projectx.auth.exception.BusinessException;
 import com.projectx.auth.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.Cipher;
